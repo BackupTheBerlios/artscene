7fb533842f5c5c04aa291aa6a573bf0b07577322
pukomuko <pukomuko>
2004-09-23 22:14:55 +0000

    kaip sito atsikratyti?

---
+++ art.scene.v2/htdocs/i_galerija.php
@@ -0,0 +1,103 @@
+<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title> senoji artscene.fluxus.lt galerija </title>
+	<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1257">
+	<link href="../css/artv2.css" rel="stylesheet" type="text/css">
+</head>
+
+<body topmargin=0 leftmargin=0 marginheight=0 marginwidth=0>
+
+<table width="100%" border=0 cellpadding=0 cellspacing=0 height="100%"><tr> <!-- outter layout -->
+	<td class="table-light" valign="top" align="left" width="769">
+	
+	
+	<table width="100%" border=0 cellpadding=0 cellspacing=0> <!-- logo virsus -->
+	<tr>
+		<td class="border-out" valign="top" align="left"><img src="../design_images/art.scene.logo.top.gif" width="374" height="10" border=0 alt=""></td>
+	</tr>
+	</table> <!-- logo virsus -->
+	
+	<table width="100%" border=0 cellpadding=0 cellspacing=0> <!-- header -->
+	<tr>
+		<td valign="top" align="left"><a href="../process/"><img src="../design_images/art.scene.logo.bottom.gif" width="377" height="59" border=0 alt=""></a><br>&nbsp;
+
+		
+		</td>
+		<td valign="bottom" align="right" rowspan=3><img src="../dot.gif" width="1" height="5" border=0 alt=""><br>
+
+
+		<img src="../design_images/header.bottom.right2.gif" width="262" height="46" border=0 alt=""><!-- <img src="../design_images/header.bottom.right.gif" width="262" height="103" border=0 alt=""> --></td>
+	</tr>
+	</table> <!-- header -->
+
+	<table width="100%" border=0 cellpadding=0 cellspacing=0> <!-- linija -->
+	<tr>
+		<td class="border-out" valign="top" align="left"><img src="../dot.gif" width="769" height="10" border=0 alt=""></td>
+	</tr>
+	</table> <!-- linija -->
+	
+	
+	<table width="100%" border=0 cellpadding=0 cellspacing=0 height="*"> <!-- two column -->
+	<tr>
+		<td width="41" align="left" valign="top"><img src="../design_images/content.top.left.gif" width="41" height="245" border=0 alt=""></td>
+		<td valign="top" align="left"> <!-- CONTENT -->
+		<img src="../dot.gif" width="1" height="3" border=0 alt=""><br>
+		<span class="title">senoji artscene.fluxus.lt galerija</span>
+		<br><br>
+		
+		čia geresni darbai pakliuvę į art.scene iki 2001 žiemos.<br><br>
+
+<? include("scripts/in_galerija_hirez.php3"); ?>
+
+<br>
+<span class="title">pagal autorių</span><br>
+<br>
+<? include("scripts/in_hirez_names.php3"); ?>
+<br>
+
+
+		</td><!-- CONTENT -->
+		<td><img src="../dot.gif" width="15" height="3" border=0 alt=""></td>
+	</tr>
+	</table> <!-- two column -->
+	
+	</td><!-- vidines celles pabaiga -->
+
+	<td class="border-out" valign="top" align="left" width="*"><img src="../dot.gif" width="1" height="1" border=0 alt=""></td><!-- isorine celle -->
+
+</tr>
+
+
+
+<tr>
+<td class="table-light" valign="bottom">
+
+	<table width="100%" border=0 cellpadding=0 cellspacing=0> <!-- linija -->
+	<tr>
+		<td class="border-out" valign="top" align="left"><img src="../dot.gif" width="769" height="10" border=0 alt=""></td>
+	</tr>
+	</table> <!-- linija -->
+
+	<table width="100%" border=0 cellpadding=0 cellspacing=0> <!-- kopyraitai ir butonai -->
+	<tr>
+		<td valign="top" align="left" class="copyright"><img src="../design_images/bottom.left.gif" width="99" height="27" border=0 alt=""></td>
+		<td valign="top" align="right" class="copyright">
+
+			
+
+		</td>
+		<td class="copyright"><img src="../dot.gif" width="5" height="10" border=0 alt=""></td>
+	</tr>
+	</table></td>
+
+<td class="border-out" valign="top" align="left" width="*"><img src="../dot.gif" width="1" height="1" border=0 alt=""></td>
+
+</tr>
+
+</table> <!-- outter layout -->
+
+
+
+</body>
+</html>
