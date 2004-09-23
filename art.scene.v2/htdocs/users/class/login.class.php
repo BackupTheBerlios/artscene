04b0967b7d5b3f5a9c6f111cf78788e17418f329
pukomuko <pukomuko>
2004-09-23 21:42:19 +0000

    import

--- art.scene.v2/htdocs/users/class/login.class.php
+++ art.scene.v2/htdocs/users/class/login.class.php
@@ -30,6 +30,7 @@ include_once($RELPATH . $COREPATH . 'avcolumn.class.php');
 
 class login extends avColumn
 {
+	var $version = '$Id: login.class.php,v 1.2 2004/09/23 21:42:19 pukomuko Exp $';
 
 	var $table = 'u_users';
 
