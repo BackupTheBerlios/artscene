855de9ed0467e432d5b60f4bec4ac1f8a171a795
pukomuko <pukomuko>
2011-07-04 21:21:02 +0000

    sql support scripts

--- art.scene.v2/scripts/sql/cleanup_users.sql
+++ art.scene.v2/scripts/sql/cleanup_users.sql
@@ -6,4 +6,20 @@ UPDATE u_users
 		forgotten_pass = '', 
 		auto_login = ''
 ;
+
+
+INSERT INTO avworks_stat (subject, work_id)
+	SELECT subject, id
+	FROM avworks;
+
+UPDATE avworks_stat st, avworks w
+	SET st.submiter = w.submiter
+	WHERE st.work_id = w.id;
 		
+INSERT INTO u_user_info (uid) 
+	SELECT u_users.id
+	FROM u_users;
+	
+	
+ALTER TABLE `u_user_info` ADD UNIQUE  `uid` (  `uid` );
+
